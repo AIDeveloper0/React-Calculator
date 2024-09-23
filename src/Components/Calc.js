b10b8be788adf550ace440cc01ea8638c1e9b19c
AIDeveloper0 <mtalha.aideveloper@gmail.com>
2024-09-23 13:29:22 +0500

    Made buttons Clickable

--- src/Components/Calc.js
+++ src/Components/Calc.js
@@ -1,29 +1,36 @@
 import React from "react";
 
 export default function Calc(){
+function handleNumber(){
+alert("Clicked");
+}
+function handleOperator(){
+
+}
+
   return(<div className="calculator">
     <div className="display">0</div>
-  <Buttons value="7"/>
-  <Buttons value="8"/>
-  <Buttons value="9"/>
-  <Buttons value="/"/>
+  <Buttons value="7" onClick={handleNumber} />
+  <Buttons value="8"onClick={handleNumber}/>
+  <Buttons value="9"onClick={handleNumber}/>
+  <Buttons value="/"className="operator" onClick={handleOperator}/>
 
-  <Buttons value="4"/>
-  <Buttons value="5"/>
-  <Buttons value="6"/>
-  <Buttons value="*"/>
+  <Buttons value="4"onClick={handleNumber}/>
+  <Buttons value="5"onClick={handleNumber}/>
+  <Buttons value="6"onClick={handleNumber}/>
+  <Buttons value="*"className="operator"onClick={handleOperator}/>
 
-  <Buttons value="1"/>
-  <Buttons value="2"/>
-  <Buttons value="3"/>
-  <Buttons value="-"/>
+  <Buttons value="1"onClick={handleNumber}/>
+  <Buttons value="2"onClick={handleNumber}/>
+  <Buttons value="3"onClick={handleNumber}/>
+  <Buttons value="-"className="operator"onClick={handleOperator}/>
 
-  <Buttons value="C"/>
-  <Buttons value="0"/>
-  <Buttons value="="/>
-  <Buttons value="+"/>
+  <Buttons value="C"onClick={handleNumber}/>
+  <Buttons value="0"onClick={handleNumber}/>
+  <Buttons value="="onClick={handleNumber}/>
+  <Buttons value="+"className="operator"onClick={handleOperator}/>
   </div>);
 }
 function Buttons(props){
-  return <button>{props.value}</button>
+  return <button className={props.className} onClick={props.onClick}>{props.value}</button>
 }
